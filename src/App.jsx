cb9b86f4ad47b2212dcb47343c342f2add4c7c00
Nathan Rangel <nathanpk20@gmail.com>
2023-11-22 20:18:48 -0300

    inicio project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+import HomePage from "./pages/HomePage"
+import SeatsPage from "./pages/SeatsPage/SeatsPage"
+import SessionsPage from "./pages/SessionsPage/SessionsPage"
+import SuccessPage from "./pages/SuccessPage/SuccessPage"
+import { useState } from "react"
+import axios from 'axios';
+
+
+export default function App() {
+
+}
+
+export default App;
