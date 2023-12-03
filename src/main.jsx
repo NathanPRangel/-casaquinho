424feb29e179cf9fd0d539aeff510bde71c82253
Nathan Rangel <nathanpk20@gmail.com>
2023-12-03 14:36:24 -0300

    ajuste deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import GlobalStyle from './styles/GlobalStyle.jsx'
+import ResetStyle from './style/ResetStyle.js'
+import GlobalStyle from './style/GlobalStyle.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyle/>
+    <ResetStyle />
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
