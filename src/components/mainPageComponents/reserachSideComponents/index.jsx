3979dffefae211efcb290efc8adf8f6287147f6b
Nathan Rangel <nathanpk20@gmail.com>
2023-12-03 14:34:10 -0300

    alterando components

--- src/components/mainPageComponents/reserachSideComponents/index.jsx
+++
@@ -1,9 +0,0 @@
-export * from './Title'
-export * from './ResearchAndInformation'
-export * from './InformationsReserachSide'
-export * from './SearchSide'
-export * from './FormSearch'
-export * from  './Timestamp'
-export * from  './ButtonsController'
-export * from  './ButtonDark'
-
--- src/components/mainPageComponents/visualSideComponents/index.jsx
+++
@@ -1,6 +0,0 @@
-export * from './MapOrInformations'
-export * from './VisualSide'
-export * from './FeaturesWeather'
-export * from './TodayOrNext'
-export * from './NameAndLocation'
-export * from './NextDays'
